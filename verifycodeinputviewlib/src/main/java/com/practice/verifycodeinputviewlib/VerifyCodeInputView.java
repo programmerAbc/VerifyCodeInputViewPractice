0ccb511fe6a336d145479d158c9e61f06e1e812d
consul <2301488793@qq.com>
2017-10-12 11:26:27 +0800

    backup

--- verifycodeinputviewlib/src/main/java/com/practice/verifycodeinputviewlib/VerifyCodeInputView.java
+++ verifycodeinputviewlib/src/main/java/com/practice/verifycodeinputviewlib/VerifyCodeInputView.java
@@ -1,11 +1,8 @@
 package com.practice.verifycodeinputviewlib;
 
 import android.content.Context;
-import android.text.Editable;
 import android.text.TextUtils;
-import android.text.TextWatcher;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -15,6 +12,7 @@ import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import java.util.LinkedList;
 import java.util.List;
 
 
@@ -24,12 +22,7 @@ import java.util.List;
 
 public class VerifyCodeInputView extends FrameLayout implements View.OnFocusChangeListener, View.OnKeyListener {
     public static final String TAG = VerifyCodeInputView.class.getSimpleName();
-    EditText vcEt0;
-    EditText vcEt1;
-    EditText vcEt2;
-    EditText vcEt3;
-    EditText vcEt4;
-    EditText vcEt5;
+    List<EditText> vcEtList;
     TextView vcEmptyView;
 
     public VerifyCodeInputView(Context context) {
@@ -49,255 +42,114 @@ public class VerifyCodeInputView extends FrameLayout implements View.OnFocusChan
 
     public void init() {
         LayoutInflater.from(getContext()).inflate(R.layout.verify_code_input_view, this, true);
-        vcEt0 = (EditText) findViewById(R.id.vc_et0);
-        vcEt1 = (EditText) findViewById(R.id.vc_et1);
-        vcEt2 = (EditText) findViewById(R.id.vc_et2);
-        vcEt3 = (EditText) findViewById(R.id.vc_et3);
-        vcEt4 = (EditText) findViewById(R.id.vc_et4);
-        vcEt5 = (EditText) findViewById(R.id.vc_et5);
+        vcEtList = new LinkedList<>();
+        vcEtList.add((EditText) findViewById(R.id.vc_et0));
+        vcEtList.add((EditText) findViewById(R.id.vc_et1));
+        vcEtList.add((EditText) findViewById(R.id.vc_et2));
+        vcEtList.add((EditText) findViewById(R.id.vc_et3));
+        vcEtList.add((EditText) findViewById(R.id.vc_et4));
+        vcEtList.add((EditText) findViewById(R.id.vc_et5));
         vcEmptyView = (TextView) findViewById(R.id.vc_emptyView);
         initOnFocusChangeListener();
         initOnKeyListener();
     }
 
     private void initOnKeyListener() {
-        vcEt0.setOnKeyListener(this);
-        vcEt1.setOnKeyListener(this);
-        vcEt2.setOnKeyListener(this);
-        vcEt3.setOnKeyListener(this);
-        vcEt4.setOnKeyListener(this);
-        vcEt5.setOnKeyListener(this);
+        for (EditText editText : vcEtList) {
+            editText.setOnKeyListener(this);
+        }
     }
 
     private void initOnFocusChangeListener() {
-        vcEt0.setOnFocusChangeListener(this);
-        vcEt1.setOnFocusChangeListener(this);
-        vcEt2.setOnFocusChangeListener(this);
-        vcEt3.setOnFocusChangeListener(this);
-        vcEt4.setOnFocusChangeListener(this);
-        vcEt5.setOnFocusChangeListener(this);
+        for (EditText editText : vcEtList) {
+            editText.setOnFocusChangeListener(this);
+        }
     }
 
     @Override
     public void onFocusChange(View v, boolean hasFocus) {
-        if (!vcEt0.hasFocus() && !vcEt1.hasFocus() && !vcEt2.hasFocus() && !vcEt3.hasFocus() && !vcEt4.hasFocus() && !vcEt5.hasFocus()) {
-            if (TextUtils.isEmpty(vcEt0.getText().toString()) &&
-                    TextUtils.isEmpty(vcEt1.getText().toString()) &&
-                    TextUtils.isEmpty(vcEt2.getText().toString()) &&
-                    TextUtils.isEmpty(vcEt3.getText().toString()) &&
-                    TextUtils.isEmpty(vcEt4.getText().toString()) &&
-                    TextUtils.isEmpty(vcEt5.getText().toString())
-                    ) {
-                vcEmptyView.setVisibility(VISIBLE);
-            } else {
+        for (EditText editText : vcEtList) {
+            if (editText.hasFocus()) {
                 vcEmptyView.setVisibility(INVISIBLE);
+                editText.setSelection(editText.getText().length());
+                return;
             }
-        } else {
-            vcEmptyView.setVisibility(INVISIBLE);
         }
+        for (EditText editText : vcEtList) {
+            if (!TextUtils.isEmpty(editText.getText().toString())) {
+                vcEmptyView.setVisibility(INVISIBLE);
+                return;
+            }
+        }
+        vcEmptyView.setVisibility(VISIBLE);
     }
 
 
     @Override
     public boolean onKey(View v, int keyCode, KeyEvent event) {
-        if (event.getAction() != KeyEvent.ACTION_UP) return true;
-        if (v.getId() == R.id.vc_et0) {
-            EditText et = (EditText) v;
-            if (!TextUtils.isEmpty(et.getText().toString())) {
-                switch (keyCode) {
-                    case KeyEvent.KEYCODE_DEL:
-                        et.setText("");
-                        break;
-                    default:
-                        break;
-                }
-            } else {
-                switch (keyCode) {
-                    case KeyEvent.KEYCODE_0:
-                    case KeyEvent.KEYCODE_1:
-                    case KeyEvent.KEYCODE_2:
-                    case KeyEvent.KEYCODE_3:
-                    case KeyEvent.KEYCODE_4:
-                    case KeyEvent.KEYCODE_5:
-                    case KeyEvent.KEYCODE_6:
-                    case KeyEvent.KEYCODE_7:
-                    case KeyEvent.KEYCODE_8:
-                    case KeyEvent.KEYCODE_9:
-                        et.setText(Character.toString((char) event.getUnicodeChar()));
-                        vcEt1.requestFocus();
-                        break;
-                }
-            }
+        for (int i = 0; i < vcEtList.size(); ++i) {
+            if (v.getId() == vcEtList.get(i).getId()) {
+                if (keyCode == KeyEvent.KEYCODE_BACK) return false;
+                if (event.getAction() != KeyEvent.ACTION_UP) return true;
 
-        } else if (v.getId() == R.id.vc_et1) {
-            EditText et = (EditText) v;
-            if (!TextUtils.isEmpty(et.getText().toString())) {
-                switch (keyCode) {
-                    case KeyEvent.KEYCODE_DEL:
-                        et.setText("");
-                        break;
-                    default:
-                        break;
-                }
-            } else {
-                switch (keyCode) {
-                    case KeyEvent.KEYCODE_0:
-                    case KeyEvent.KEYCODE_1:
-                    case KeyEvent.KEYCODE_2:
-                    case KeyEvent.KEYCODE_3:
-                    case KeyEvent.KEYCODE_4:
-                    case KeyEvent.KEYCODE_5:
-                    case KeyEvent.KEYCODE_6:
-                    case KeyEvent.KEYCODE_7:
-                    case KeyEvent.KEYCODE_8:
-                    case KeyEvent.KEYCODE_9:
-                        et.setText(Character.toString((char) event.getUnicodeChar()));
-                        vcEt2.requestFocus();
-                        break;
-                    case KeyEvent.KEYCODE_DEL:
-                        vcEt0.requestFocus();
-                        vcEt0.setSelection(vcEt0.getText().length());
-                        break;
-                    default:
-                        break;
-                }
-            }
-        } else if (v.getId() == R.id.vc_et2) {
-            EditText et = (EditText) v;
-            if (!TextUtils.isEmpty(et.getText().toString())) {
-                switch (keyCode) {
-                    case KeyEvent.KEYCODE_DEL:
-                        et.setText("");
-                        break;
-                    default:
-                        break;
-                }
-            } else {
-                switch (keyCode) {
-                    case KeyEvent.KEYCODE_0:
-                    case KeyEvent.KEYCODE_1:
-                    case KeyEvent.KEYCODE_2:
-                    case KeyEvent.KEYCODE_3:
-                    case KeyEvent.KEYCODE_4:
-                    case KeyEvent.KEYCODE_5:
-                    case KeyEvent.KEYCODE_6:
-                    case KeyEvent.KEYCODE_7:
-                    case KeyEvent.KEYCODE_8:
-                    case KeyEvent.KEYCODE_9:
-                        et.setText(Character.toString((char) event.getUnicodeChar()));
-                        vcEt3.requestFocus();
-                        break;
-                    case KeyEvent.KEYCODE_DEL:
-                        vcEt1.requestFocus();
-                        vcEt1.setSelection(vcEt1.getText().length());
-                        break;
-                    default:
-                        break;
-                }
-            }
-        } else if (v.getId() == R.id.vc_et3) {
-            EditText et = (EditText) v;
-            if (!TextUtils.isEmpty(et.getText().toString())) {
-                switch (keyCode) {
+                EditText editText = (EditText) v;
+                if (TextUtils.isEmpty(editText.getText().toString())) {
+                    switch (keyCode) {
+                        case KeyEvent.KEYCODE_0:
+                        case KeyEvent.KEYCODE_1:
+                        case KeyEvent.KEYCODE_2:
+                        case KeyEvent.KEYCODE_3:
+                        case KeyEvent.KEYCODE_4:
+                        case KeyEvent.KEYCODE_5:
+                        case KeyEvent.KEYCODE_6:
+                        case KeyEvent.KEYCODE_7:
+                        case KeyEvent.KEYCODE_8:
+                        case KeyEvent.KEYCODE_9:
+                            editText.setText(Character.toString((char) event.getUnicodeChar()));
+                            editText.setSelection(editText.getText().length());
+                            if (i < (vcEtList.size() - 1)) {
+                                vcEtList.get(i + 1).requestFocus();
+                            }
+                            break;
+                        case KeyEvent.KEYCODE_DEL:
+                            if (i > 0) {
+                                vcEtList.get(i - 1).requestFocus();
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                } else {
+                    switch (keyCode) {
+                        case KeyEvent.KEYCODE_0:
+                        case KeyEvent.KEYCODE_1:
+                        case KeyEvent.KEYCODE_2:
+                        case KeyEvent.KEYCODE_3:
+                        case KeyEvent.KEYCODE_4:
+                        case KeyEvent.KEYCODE_5:
+                        case KeyEvent.KEYCODE_6:
+                        case KeyEvent.KEYCODE_7:
+                        case KeyEvent.KEYCODE_8:
+                        case KeyEvent.KEYCODE_9:
+                            editText.setText(Character.toString((char) event.getUnicodeChar()));
+                            if (i < (vcEtList.size() - 1)) {
+                                vcEtList.get(i + 1).requestFocus();
+                            } else {
+                                editText.setSelection(editText.getText().length());
+                            }
+                            break;
+                        case KeyEvent.KEYCODE_DEL:
+                            editText.setText("");
+                            break;
 
-                    case KeyEvent.KEYCODE_DEL:
-                        et.setText("");
-                        break;
-                    default:
-                        break;
+                        default:
+                            break;
+                    }
                 }
-            } else {
-                switch (keyCode) {
-                    case KeyEvent.KEYCODE_0:
-                    case KeyEvent.KEYCODE_1:
-                    case KeyEvent.KEYCODE_2:
-                    case KeyEvent.KEYCODE_3:
-                    case KeyEvent.KEYCODE_4:
-                    case KeyEvent.KEYCODE_5:
-                    case KeyEvent.KEYCODE_6:
-                    case KeyEvent.KEYCODE_7:
-                    case KeyEvent.KEYCODE_8:
-                    case KeyEvent.KEYCODE_9:
-                        et.setText(Character.toString((char) event.getUnicodeChar()));
-                        vcEt4.requestFocus();
-                        break;
-                    case KeyEvent.KEYCODE_DEL:
-                        vcEt2.requestFocus();
-                        vcEt2.setSelection(vcEt2.getText().length());
-                        break;
-                    default:
-                        break;
-                }
-            }
-        } else if (v.getId() == R.id.vc_et4) {
-            EditText et = (EditText) v;
-            if (!TextUtils.isEmpty(et.getText().toString())) {
-                switch (keyCode) {
-
-                    case KeyEvent.KEYCODE_DEL:
-                        et.setText("");
-                        break;
-                    default:
-                        break;
-                }
-            } else {
-                switch (keyCode) {
-                    case KeyEvent.KEYCODE_0:
-                    case KeyEvent.KEYCODE_1:
-                    case KeyEvent.KEYCODE_2:
-                    case KeyEvent.KEYCODE_3:
-                    case KeyEvent.KEYCODE_4:
-                    case KeyEvent.KEYCODE_5:
-                    case KeyEvent.KEYCODE_6:
-                    case KeyEvent.KEYCODE_7:
-                    case KeyEvent.KEYCODE_8:
-                    case KeyEvent.KEYCODE_9:
-                        et.setText(Character.toString((char) event.getUnicodeChar()));
-                        vcEt5.requestFocus();
-                        break;
-                    case KeyEvent.KEYCODE_DEL:
-                        vcEt3.requestFocus();
-                        vcEt3.setSelection(vcEt3.getText().length());
-                        break;
-                    default:
-                        break;
-                }
-            }
-        } else if (v.getId() == R.id.vc_et5) {
-            EditText et = (EditText) v;
-            if (!TextUtils.isEmpty(et.getText().toString())) {
-                switch (keyCode) {
-                    case KeyEvent.KEYCODE_DEL:
-                        et.setText("");
-                        break;
-                    default:
-                        break;
-                }
-            } else {
-                switch (keyCode) {
-                    case KeyEvent.KEYCODE_0:
-                    case KeyEvent.KEYCODE_1:
-                    case KeyEvent.KEYCODE_2:
-                    case KeyEvent.KEYCODE_3:
-                    case KeyEvent.KEYCODE_4:
-                    case KeyEvent.KEYCODE_5:
-                    case KeyEvent.KEYCODE_6:
-                    case KeyEvent.KEYCODE_7:
-                    case KeyEvent.KEYCODE_8:
-                    case KeyEvent.KEYCODE_9:
-                        et.setText(Character.toString((char) event.getUnicodeChar()));
-                        et.setSelection(et.getText().length());
-                        break;
-                    case KeyEvent.KEYCODE_DEL:
-                        vcEt4.requestFocus();
-                        vcEt4.setSelection(vcEt4.getText().length());
-                        break;
-                    default:
-                        break;
-                }
+                return true;
             }
         }
-        return true;
+        return false;
     }
 
     @Override
@@ -308,24 +160,18 @@ public class VerifyCodeInputView extends FrameLayout implements View.OnFocusChan
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            if (TextUtils.isEmpty(vcEt0.getText().toString())) {
-                vcEt0.requestFocus();
-                ((InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(vcEt0, 0);
-            } else if (TextUtils.isEmpty(vcEt1.getText().toString())) {
-                vcEt1.requestFocus();
-                ((InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(vcEt1, 0);
-            } else if (TextUtils.isEmpty(vcEt2.getText().toString())) {
-                vcEt2.requestFocus();
-                ((InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(vcEt2, 0);
-            } else if (TextUtils.isEmpty(vcEt3.getText().toString())) {
-                vcEt3.requestFocus();
-                ((InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(vcEt3, 0);
-            } else if (TextUtils.isEmpty(vcEt4.getText().toString())) {
-                vcEt4.requestFocus();
-                ((InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(vcEt4, 0);
-            } else {
-                vcEt5.requestFocus();
-                ((InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(vcEt5, 0);
+            for (int i = vcEtList.size() - 1; i >= 0; --i) {
+                if (!TextUtils.isEmpty(vcEtList.get(i).getText().toString())) {
+                    vcEtList.get(Math.max(0, Math.min(i + 1, vcEtList.size() - 1))).requestFocus();
+                    ((InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(vcEtList.get(Math.max(0, Math.min(i + 1, vcEtList.size() - 1))), 0);
+                    break;
+                } else {
+                    if (i == 0) {
+                        vcEtList.get(0).requestFocus();
+                        ((InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(vcEtList.get(0), 0);
+                        break;
+                    }
+                }
             }
         }
         return true;
