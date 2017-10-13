8dadbbcb1349553e6734294e69a8eeb5c803c001
consul <2301488793@qq.com>
2017-10-13 17:51:50 +0800

    最终采用这种方案

--- verifycodeinputviewlib/src/main/java/com/practice/verifycodeinputviewlib/VerifyCodeInputView.java
+++ verifycodeinputviewlib/src/main/java/com/practice/verifycodeinputviewlib/VerifyCodeInputView.java
@@ -1,9 +1,9 @@
 package com.practice.verifycodeinputviewlib;
 
 import android.content.Context;
-import android.text.TextUtils;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -20,10 +20,11 @@ import java.util.List;
  * Created by zhuyakun on 2017/10/11.
  */
 
-public class VerifyCodeInputView extends FrameLayout implements View.OnFocusChangeListener, VerifyCodeEditText.KeyboardListener {
+public class VerifyCodeInputView extends FrameLayout {
     public static final String TAG = VerifyCodeInputView.class.getSimpleName();
-    List<VerifyCodeEditText> vcEtList;
-    TextView vcEmptyView;
+    List<TextView> vcTvList;
+    List<View> vcUlList;
+    EditText vcInputEt;
 
     public VerifyCodeInputView(Context context) {
         super(context);
@@ -42,136 +43,87 @@ public class VerifyCodeInputView extends FrameLayout implements View.OnFocusChan
 
     public void init() {
         LayoutInflater.from(getContext()).inflate(R.layout.verify_code_input_view, this, true);
-        vcEtList = new LinkedList<>();
-        vcEtList.add((VerifyCodeEditText) findViewById(R.id.vc_et0));
-        vcEtList.add((VerifyCodeEditText) findViewById(R.id.vc_et1));
-        vcEtList.add((VerifyCodeEditText) findViewById(R.id.vc_et2));
-        vcEtList.add((VerifyCodeEditText) findViewById(R.id.vc_et3));
-        vcEtList.add((VerifyCodeEditText) findViewById(R.id.vc_et4));
-        vcEtList.add((VerifyCodeEditText) findViewById(R.id.vc_et5));
-        vcEmptyView = (TextView) findViewById(R.id.vc_emptyView);
-        initOnFocusChangeListener();
-        initKeyboardListener();
-    }
+        vcTvList = new LinkedList<>();
+        vcUlList = new LinkedList<>();
+        vcTvList.add((TextView) findViewById(R.id.vc_tv0));
+        vcTvList.add((TextView) findViewById(R.id.vc_tv1));
+        vcTvList.add((TextView) findViewById(R.id.vc_tv2));
+        vcTvList.add((TextView) findViewById(R.id.vc_tv3));
+        vcTvList.add((TextView) findViewById(R.id.vc_tv4));
+        vcTvList.add((TextView) findViewById(R.id.vc_tv5));
+        vcUlList.add(findViewById(R.id.vc_ul0));
+        vcUlList.add(findViewById(R.id.vc_ul1));
+        vcUlList.add(findViewById(R.id.vc_ul2));
+        vcUlList.add(findViewById(R.id.vc_ul3));
+        vcUlList.add(findViewById(R.id.vc_ul4));
+        vcUlList.add(findViewById(R.id.vc_ul5));
+        vcInputEt = (EditText) findViewById(R.id.vc_inputEt);
+        vcInputEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-    private void initKeyboardListener() {
-        for (VerifyCodeEditText vcEditText : vcEtList) {
-            vcEditText.setKeyboardListener(this);
-        }
-    }
-
-    private void initOnFocusChangeListener() {
-        for (EditText editText : vcEtList) {
-            editText.setOnFocusChangeListener(this);
-        }
-    }
-
-    @Override
-    public void onFocusChange(View v, boolean hasFocus) {
-        for (EditText editText : vcEtList) {
-            if (editText.hasFocus()) {
-                vcEmptyView.setVisibility(INVISIBLE);
-                editText.setSelection(editText.getText().length());
-                return;
             }
-        }
-        for (EditText editText : vcEtList) {
-            if (!TextUtils.isEmpty(editText.getText().toString())) {
-                vcEmptyView.setVisibility(INVISIBLE);
-                return;
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
             }
-        }
-        vcEmptyView.setVisibility(VISIBLE);
-    }
 
-
-    @Override
-    public boolean onKey(VerifyCodeEditText vcEditText, int keyCode, KeyEvent event) {
-        for (int i = 0; i < vcEtList.size(); ++i) {
-            if (vcEditText.getId() == vcEtList.get(i).getId()) {
-                if (keyCode == KeyEvent.KEYCODE_BACK) return false;
-                if (event.getAction() != KeyEvent.ACTION_UP) return true;
-                if (TextUtils.isEmpty(vcEditText.getText().toString())) {
-                    switch (keyCode) {
-                        case KeyEvent.KEYCODE_0:
-                        case KeyEvent.KEYCODE_1:
-                        case KeyEvent.KEYCODE_2:
-                        case KeyEvent.KEYCODE_3:
-                        case KeyEvent.KEYCODE_4:
-                        case KeyEvent.KEYCODE_5:
-                        case KeyEvent.KEYCODE_6:
-                        case KeyEvent.KEYCODE_7:
-                        case KeyEvent.KEYCODE_8:
-                        case KeyEvent.KEYCODE_9:
-                            vcEditText.setText(Character.toString((char) event.getUnicodeChar()));
-                            vcEditText.setSelection(vcEditText.getText().length());
-                            if (i < (vcEtList.size() - 1)) {
-                                vcEtList.get(i + 1).requestFocus();
-                            }
-                            break;
-                        case KeyEvent.KEYCODE_DEL:
-                            if (i > 0) {
-                                vcEtList.get(i - 1).requestFocus();
-                            }
-                            break;
-                        default:
-                            break;
-                    }
+            @Override
+            public void afterTextChanged(Editable s) {
+                updateUnderLine();
+                updateVerifyCodeShowGrid();
+            }
+        });
+        vcInputEt.setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    updateUnderLine();
                 } else {
-                    switch (keyCode) {
-                        case KeyEvent.KEYCODE_0:
-                        case KeyEvent.KEYCODE_1:
-                        case KeyEvent.KEYCODE_2:
-                        case KeyEvent.KEYCODE_3:
-                        case KeyEvent.KEYCODE_4:
-                        case KeyEvent.KEYCODE_5:
-                        case KeyEvent.KEYCODE_6:
-                        case KeyEvent.KEYCODE_7:
-                        case KeyEvent.KEYCODE_8:
-                        case KeyEvent.KEYCODE_9:
-                            vcEditText.setText(Character.toString((char) event.getUnicodeChar()));
-                            if (i < (vcEtList.size() - 1)) {
-                                vcEtList.get(i + 1).requestFocus();
-                            } else {
-                                vcEditText.setSelection(vcEditText.getText().length());
-                            }
-                            break;
-                        case KeyEvent.KEYCODE_DEL:
-                            vcEditText.setText("");
-                            break;
-
-                        default:
-                            break;
-                    }
+                    resetUnderLine();
                 }
-                return true;
             }
-        }
-        return false;
+        });
+        resetUnderLine();
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            vcInputEt.requestFocus();
+            vcInputEt.setSelection(vcInputEt.getText().length());
+            ((InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(vcInputEt, 0);
+        }
         return true;
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            for (int i = vcEtList.size() - 1; i >= 0; --i) {
-                if (!TextUtils.isEmpty(vcEtList.get(i).getText().toString())) {
-                    vcEtList.get(Math.max(0, Math.min(i + 1, vcEtList.size() - 1))).requestFocus();
-                    ((InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(vcEtList.get(Math.max(0, Math.min(i + 1, vcEtList.size() - 1))), 0);
-                    break;
-                } else {
-                    if (i == 0) {
-                        vcEtList.get(0).requestFocus();
-                        ((InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(vcEtList.get(0), 0);
-                        break;
-                    }
-                }
-            }
+    public void resetVerifyCodeShowGrid() {
+        for (TextView textView : vcTvList) {
+            textView.setText("");
         }
-        return true;
+    }
+
+    public void updateVerifyCodeShowGrid() {
+        resetVerifyCodeShowGrid();
+        Editable editable = vcInputEt.getText();
+        for (int i = 0; i < editable.length(); ++i) {
+            vcTvList.get(i).setText(Character.toString(editable.charAt(i)));
+        }
+    }
+
+    public void resetUnderLine() {
+        for (View line : vcUlList) {
+            line.setEnabled(false);
+        }
+    }
+
+    public void updateUnderLine() {
+        resetUnderLine();
+        if (vcInputEt.getText().length() < vcUlList.size()) {
+            vcUlList.get(vcInputEt.getText().length()).setEnabled(true);
+        } else {
+            vcUlList.get(vcUlList.size() - 1).setEnabled(true);
+        }
     }
 }
